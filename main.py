9f10f6ca9510a771a6258bc682c0f1e49f5d801c
Danial Goodwin <danialgoodwin@gmail.com>
2020-02-04 19:53:11 -0700

    Add ability to save

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from tkinter import *
+import tkinter.filedialog
+
+
+text = None
+
+
+def save():
+    print('save()')
+    global text
+    t = text.get('1.0', 'end-1c')
+    save_location = tkinter.filedialog.asksaveasfilename()
+    with open(save_location, 'w+') as f:
+        f.write(t)
 
 
 def start():
     print('start()')
+    global text
     root = Tk('Ultimate App')
     text = Text(root)
     text.grid()
+    button = Button(root, text='Save', command=save)
+    button.grid()
     root.mainloop()
 
 
